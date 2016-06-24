c7c2cfab51be5ff82f3319539dac38e2c71e33ab
Daniel Pinheiro <dedelabritos@gmail.com>
2016-06-24 12:33:55 -0300

    assets added

---
+++ public/js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+  $('button.open-folders').click(function () {
+    $('aside.folders').toggleClass('open')
+  })
+})
---
+++ public/lib/compressed.js
@@ -0,0 +1,3 @@
+// @prepros-prepend jquery-2.2.4.min.js
+// @prepros-prepend angular.js
+// @prepros-prepend angular-sanitize.js
